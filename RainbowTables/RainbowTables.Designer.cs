dcd1eb6d22100cb07caa0a45fca6984fc40d573f
Filip Pawyza <pawyza.f@gmail.com>
2021-12-30 16:22:20 +0100

    MD5 values generation

---
+++ RainbowTables/RainbowTables.Designer.cs
@@ -0,0 +1,276 @@
+﻿
+namespace RainbowTables
+{
+    partial class RainbowTables
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.GeneratedValues_Label = new System.Windows.Forms.Label();
+            this.GenerateValues_Button = new System.Windows.Forms.Button();
+            this.GeneratedValues_Hashed_TextBox = new System.Windows.Forms.RichTextBox();
+            this.GeneratedValues_Plain_TextBox = new System.Windows.Forms.RichTextBox();
+            this.GeneratedValues_Plain_Label = new System.Windows.Forms.Label();
+            this.GeneratedValues_Hashed_Label = new System.Windows.Forms.Label();
+            this.GeneratedValuesNumber_NumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.GeneratedValuesNumber_Label = new System.Windows.Forms.Label();
+            this.Numeric_Radio = new System.Windows.Forms.CheckBox();
+            this.Alpha_Radio = new System.Windows.Forms.CheckBox();
+            this.LowerAlpha_Radio = new System.Windows.Forms.CheckBox();
+            this.Characters_Label = new System.Windows.Forms.Label();
+            this.GeneratedValuesMaximumLength_Label = new System.Windows.Forms.Label();
+            this.GeneratedValuesMaximumLength_NumericUpDown = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.GeneratedValuesNumber_NumericUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GeneratedValuesMaximumLength_NumericUpDown)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // GeneratedValues_Label
+            // 
+            this.GeneratedValues_Label.AccessibleName = "GeneratedValues_Label";
+            this.GeneratedValues_Label.AutoSize = true;
+            this.GeneratedValues_Label.Cursor = System.Windows.Forms.Cursors.No;
+            this.GeneratedValues_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.GeneratedValues_Label.Location = new System.Drawing.Point(224, 154);
+            this.GeneratedValues_Label.Name = "GeneratedValues_Label";
+            this.GeneratedValues_Label.Size = new System.Drawing.Size(159, 20);
+            this.GeneratedValues_Label.TabIndex = 2;
+            this.GeneratedValues_Label.Text = "Generated Values";
+            // 
+            // GenerateValues_Button
+            // 
+            this.GenerateValues_Button.AccessibleName = "GenerateValues_Button";
+            this.GenerateValues_Button.Enabled = false;
+            this.GenerateValues_Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.GenerateValues_Button.Location = new System.Drawing.Point(423, 52);
+            this.GenerateValues_Button.Name = "GenerateValues_Button";
+            this.GenerateValues_Button.Size = new System.Drawing.Size(167, 53);
+            this.GenerateValues_Button.TabIndex = 4;
+            this.GenerateValues_Button.Text = "Generate Values";
+            this.GenerateValues_Button.UseVisualStyleBackColor = true;
+            this.GenerateValues_Button.Click += new System.EventHandler(this.GenerateValues_Button_Click);
+            // 
+            // GeneratedValues_Hashed_TextBox
+            // 
+            this.GeneratedValues_Hashed_TextBox.AccessibleName = "GeneratedValues_Hashed_TextBox";
+            this.GeneratedValues_Hashed_TextBox.Location = new System.Drawing.Point(312, 221);
+            this.GeneratedValues_Hashed_TextBox.Name = "GeneratedValues_Hashed_TextBox";
+            this.GeneratedValues_Hashed_TextBox.Size = new System.Drawing.Size(278, 528);
+            this.GeneratedValues_Hashed_TextBox.TabIndex = 12;
+            this.GeneratedValues_Hashed_TextBox.Text = "";
+            // 
+            // GeneratedValues_Plain_TextBox
+            // 
+            this.GeneratedValues_Plain_TextBox.AccessibleName = "GeneratedValues_Plain_TextBox";
+            this.GeneratedValues_Plain_TextBox.Location = new System.Drawing.Point(12, 221);
+            this.GeneratedValues_Plain_TextBox.Name = "GeneratedValues_Plain_TextBox";
+            this.GeneratedValues_Plain_TextBox.Size = new System.Drawing.Size(278, 528);
+            this.GeneratedValues_Plain_TextBox.TabIndex = 13;
+            this.GeneratedValues_Plain_TextBox.Text = "";
+            // 
+            // GeneratedValues_Plain_Label
+            // 
+            this.GeneratedValues_Plain_Label.AccessibleName = "GeneratedValues_Plain_Label";
+            this.GeneratedValues_Plain_Label.AutoSize = true;
+            this.GeneratedValues_Plain_Label.Cursor = System.Windows.Forms.Cursors.No;
+            this.GeneratedValues_Plain_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.GeneratedValues_Plain_Label.Location = new System.Drawing.Point(120, 181);
+            this.GeneratedValues_Plain_Label.Name = "GeneratedValues_Plain_Label";
+            this.GeneratedValues_Plain_Label.Size = new System.Drawing.Size(62, 20);
+            this.GeneratedValues_Plain_Label.TabIndex = 14;
+            this.GeneratedValues_Plain_Label.Text = "PLAIN";
+            // 
+            // GeneratedValues_Hashed_Label
+            // 
+            this.GeneratedValues_Hashed_Label.AccessibleName = "GeneratedValues_Hashed_Label";
+            this.GeneratedValues_Hashed_Label.AutoSize = true;
+            this.GeneratedValues_Hashed_Label.Cursor = System.Windows.Forms.Cursors.No;
+            this.GeneratedValues_Hashed_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.GeneratedValues_Hashed_Label.Location = new System.Drawing.Point(412, 181);
+            this.GeneratedValues_Hashed_Label.Name = "GeneratedValues_Hashed_Label";
+            this.GeneratedValues_Hashed_Label.Size = new System.Drawing.Size(87, 20);
+            this.GeneratedValues_Hashed_Label.TabIndex = 15;
+            this.GeneratedValues_Hashed_Label.Text = "HASHED";
+            // 
+            // GeneratedValuesNumber_NumericUpDown
+            // 
+            this.GeneratedValuesNumber_NumericUpDown.AccessibleName = "GeneratedValuesNumber_NumericUpDown";
+            this.GeneratedValuesNumber_NumericUpDown.Location = new System.Drawing.Point(228, 47);
+            this.GeneratedValuesNumber_NumericUpDown.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.GeneratedValuesNumber_NumericUpDown.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.GeneratedValuesNumber_NumericUpDown.Name = "GeneratedValuesNumber_NumericUpDown";
+            this.GeneratedValuesNumber_NumericUpDown.Size = new System.Drawing.Size(168, 20);
+            this.GeneratedValuesNumber_NumericUpDown.TabIndex = 16;
+            this.GeneratedValuesNumber_NumericUpDown.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // GeneratedValuesNumber_Label
+            // 
+            this.GeneratedValuesNumber_Label.AccessibleName = "GeneratedValuesNumber_Label";
+            this.GeneratedValuesNumber_Label.AutoSize = true;
+            this.GeneratedValuesNumber_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.GeneratedValuesNumber_Label.Location = new System.Drawing.Point(225, 21);
+            this.GeneratedValuesNumber_Label.Name = "GeneratedValuesNumber_Label";
+            this.GeneratedValuesNumber_Label.Size = new System.Drawing.Size(171, 13);
+            this.GeneratedValuesNumber_Label.TabIndex = 17;
+            this.GeneratedValuesNumber_Label.Text = "Number of generated values:";
+            // 
+            // Numeric_Radio
+            // 
+            this.Numeric_Radio.AccessibleName = "Numeric_Radio";
+            this.Numeric_Radio.AutoSize = true;
+            this.Numeric_Radio.Location = new System.Drawing.Point(75, 49);
+            this.Numeric_Radio.Name = "Numeric_Radio";
+            this.Numeric_Radio.Size = new System.Drawing.Size(65, 17);
+            this.Numeric_Radio.TabIndex = 18;
+            this.Numeric_Radio.Text = "Numeric";
+            this.Numeric_Radio.UseVisualStyleBackColor = true;
+            this.Numeric_Radio.CheckedChanged += new System.EventHandler(this.Numeric_Radio_CheckedChanged);
+            // 
+            // Alpha_Radio
+            // 
+            this.Alpha_Radio.AccessibleName = "Alpha_Radio";
+            this.Alpha_Radio.AutoSize = true;
+            this.Alpha_Radio.Location = new System.Drawing.Point(75, 72);
+            this.Alpha_Radio.Name = "Alpha_Radio";
+            this.Alpha_Radio.Size = new System.Drawing.Size(53, 17);
+            this.Alpha_Radio.TabIndex = 19;
+            this.Alpha_Radio.Text = "Alpha";
+            this.Alpha_Radio.UseVisualStyleBackColor = true;
+            this.Alpha_Radio.CheckedChanged += new System.EventHandler(this.Alpha_Radio_CheckedChanged);
+            // 
+            // LowerAlpha_Radio
+            // 
+            this.LowerAlpha_Radio.AccessibleName = "LowerAlpha_Radio";
+            this.LowerAlpha_Radio.AutoSize = true;
+            this.LowerAlpha_Radio.Location = new System.Drawing.Point(75, 95);
+            this.LowerAlpha_Radio.Name = "LowerAlpha_Radio";
+            this.LowerAlpha_Radio.Size = new System.Drawing.Size(85, 17);
+            this.LowerAlpha_Radio.TabIndex = 20;
+            this.LowerAlpha_Radio.Text = "Lower Alpha";
+            this.LowerAlpha_Radio.UseVisualStyleBackColor = true;
+            this.LowerAlpha_Radio.CheckedChanged += new System.EventHandler(this.LowerAlpha_Radio_CheckedChanged);
+            // 
+            // Characters_Label
+            // 
+            this.Characters_Label.AutoSize = true;
+            this.Characters_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Characters_Label.Location = new System.Drawing.Point(72, 28);
+            this.Characters_Label.Name = "Characters_Label";
+            this.Characters_Label.Size = new System.Drawing.Size(111, 13);
+            this.Characters_Label.TabIndex = 21;
+            this.Characters_Label.Text = "Characters to use:";
+            // 
+            // GeneratedValuesMaximumLength_Label
+            // 
+            this.GeneratedValuesMaximumLength_Label.AccessibleName = "GeneratedValuesMaximumLength_Label";
+            this.GeneratedValuesMaximumLength_Label.AutoSize = true;
+            this.GeneratedValuesMaximumLength_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.GeneratedValuesMaximumLength_Label.Location = new System.Drawing.Point(225, 77);
+            this.GeneratedValuesMaximumLength_Label.Name = "GeneratedValuesMaximumLength_Label";
+            this.GeneratedValuesMaximumLength_Label.Size = new System.Drawing.Size(137, 13);
+            this.GeneratedValuesMaximumLength_Label.TabIndex = 23;
+            this.GeneratedValuesMaximumLength_Label.Text = "Maximum Value Length";
+            // 
+            // GeneratedValuesMaximumLength_NumericUpDown
+            // 
+            this.GeneratedValuesMaximumLength_NumericUpDown.AccessibleName = "GeneratedValuesMaximumLength_NumericUpDown";
+            this.GeneratedValuesMaximumLength_NumericUpDown.Location = new System.Drawing.Point(228, 103);
+            this.GeneratedValuesMaximumLength_NumericUpDown.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.GeneratedValuesMaximumLength_NumericUpDown.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.GeneratedValuesMaximumLength_NumericUpDown.Name = "GeneratedValuesMaximumLength_NumericUpDown";
+            this.GeneratedValuesMaximumLength_NumericUpDown.Size = new System.Drawing.Size(168, 20);
+            this.GeneratedValuesMaximumLength_NumericUpDown.TabIndex = 22;
+            this.GeneratedValuesMaximumLength_NumericUpDown.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // RainbowTables
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(602, 761);
+            this.Controls.Add(this.GeneratedValuesMaximumLength_Label);
+            this.Controls.Add(this.GeneratedValuesMaximumLength_NumericUpDown);
+            this.Controls.Add(this.Characters_Label);
+            this.Controls.Add(this.LowerAlpha_Radio);
+            this.Controls.Add(this.Alpha_Radio);
+            this.Controls.Add(this.Numeric_Radio);
+            this.Controls.Add(this.GeneratedValuesNumber_Label);
+            this.Controls.Add(this.GeneratedValuesNumber_NumericUpDown);
+            this.Controls.Add(this.GeneratedValues_Hashed_Label);
+            this.Controls.Add(this.GeneratedValues_Plain_Label);
+            this.Controls.Add(this.GeneratedValues_Plain_TextBox);
+            this.Controls.Add(this.GeneratedValues_Hashed_TextBox);
+            this.Controls.Add(this.GenerateValues_Button);
+            this.Controls.Add(this.GeneratedValues_Label);
+            this.Name = "RainbowTables";
+            this.Text = "RainbowTables";
+            ((System.ComponentModel.ISupportInitialize)(this.GeneratedValuesNumber_NumericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.GeneratedValuesMaximumLength_NumericUpDown)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label GeneratedValues_Label;
+        private System.Windows.Forms.Button GenerateValues_Button;
+        private System.Windows.Forms.RichTextBox GeneratedValues_Hashed_TextBox;
+        private System.Windows.Forms.RichTextBox GeneratedValues_Plain_TextBox;
+        private System.Windows.Forms.Label GeneratedValues_Plain_Label;
+        private System.Windows.Forms.Label GeneratedValues_Hashed_Label;
+        private System.Windows.Forms.NumericUpDown GeneratedValuesNumber_NumericUpDown;
+        private System.Windows.Forms.Label GeneratedValuesNumber_Label;
+        private System.Windows.Forms.CheckBox Numeric_Radio;
+        private System.Windows.Forms.CheckBox Alpha_Radio;
+        private System.Windows.Forms.CheckBox LowerAlpha_Radio;
+        private System.Windows.Forms.Label Characters_Label;
+        private System.Windows.Forms.Label GeneratedValuesMaximumLength_Label;
+        private System.Windows.Forms.NumericUpDown GeneratedValuesMaximumLength_NumericUpDown;
+    }
+}
+
---
+++ RainbowTables/RainbowTables.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RainbowTables
+{
+    public partial class RainbowTables : Form
+    {
+        UnicodeEncoding UE;
+        MD5CryptoServiceProvider MD5hash;
+        Random random;
+        const String NUMERIC = "0123456789";
+        const String ALPHA = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        const String LOWERALPHA = "abcdefghijklmnopqrstuvwxyz";
+
+
+        public RainbowTables()
+        {
+            UE = new UnicodeEncoding();
+            random = new Random();
+            MD5hash = new MD5CryptoServiceProvider();
+            InitializeComponent();
+        }
+
+        private void GenerateValues_Button_Click(object sender, EventArgs e)
+        {
+            List<String> plainValues = GenerateRandomStringList((int) GeneratedValuesNumber_NumericUpDown.Value, (int) GeneratedValuesMaximumLength_NumericUpDown.Value);
+            List<String> hashedValues = MD5HashStringList(plainValues);
+            GeneratedValues_Plain_TextBox.Text = string.Join("\r\n", plainValues);
+            GeneratedValues_Hashed_TextBox.Text = string.Join("\r\n", hashedValues);
+        }
+
+        private List<String> GenerateRandomStringList(int randomValuesNumber, int maximumValueLength)
+        {
+            List<String> randomValues = new List<String>();
+            String characters = GetAvailableCharacters();
+            for (int i = 0; i < randomValuesNumber; i++)
+            {
+                StringBuilder generatigValue = new StringBuilder();
+
+                for (int j = 0; j < random.Next(1, maximumValueLength); j++)
+                {
+                    int charIndex = random.Next(0, characters.Length);
+                    Console.WriteLine(charIndex);
+                    generatigValue.Append(characters.Substring(charIndex, 1));
+                }
+                randomValues.Add(generatigValue.ToString());
+            }
+            return randomValues;
+        }
+
+        private String GetAvailableCharacters() {
+            String characters = "";
+            characters += Numeric_Radio.Checked ? NUMERIC : "";
+            characters += Alpha_Radio.Checked ? ALPHA : "";
+            characters += LowerAlpha_Radio.Checked ? LOWERALPHA : "";
+            return characters;
+            }
+        private List<String> MD5HashStringList(List<String> plainValues)
+        {
+            List<String> hashedValues = new List<String>();
+            foreach (String value in plainValues)
+            {
+                byte[] valueInBytes = UE.GetBytes(value);
+                byte[] valueHashed = MD5hash.ComputeHash(valueInBytes);
+                hashedValues.Add(BitConverter.ToString(valueHashed).Replace("-", "").ToLowerInvariant());
+            }
+            return hashedValues;
+        }
+
+        private void Numeric_Radio_CheckedChanged(object sender, EventArgs e)
+        {
+            GenerateValues_Button.Enabled = Numeric_Radio.Checked || Alpha_Radio.Checked || LowerAlpha_Radio.Checked;
+        }
+
+        private void Alpha_Radio_CheckedChanged(object sender, EventArgs e)
+        {
+            GenerateValues_Button.Enabled = Numeric_Radio.Checked || Alpha_Radio.Checked || LowerAlpha_Radio.Checked;
+        }
+
+        private void LowerAlpha_Radio_CheckedChanged(object sender, EventArgs e)
+        {
+            GenerateValues_Button.Enabled = Numeric_Radio.Checked || Alpha_Radio.Checked || LowerAlpha_Radio.Checked;
+        }
+    }
+}
